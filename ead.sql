bc0849f31fb1c3829a4fe1c375016647504e376d
Fernando Campos <nandomp3@gmail.com>
2020-02-24 20:27:28 -0300

    Adicao do arquivo de banco de dados

---
+++ ead.sql
@@ -0,0 +1,297 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Tempo de geração: 24-Fev-2020 às 23:22
+-- Versão do servidor: 5.7.28
+-- versão do PHP: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `ead`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `admins`
+--
+
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `admins`
+--
+
+INSERT INTO `admins` (`id`, `email`, `senha`) VALUES
+(1, 'fernando@teste.com', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+DROP TABLE IF EXISTS `alunos`;
+CREATE TABLE IF NOT EXISTS `alunos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `alunos`
+--
+
+INSERT INTO `alunos` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Fernando', 'fernando@teste.com', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aluno_curso`
+--
+
+DROP TABLE IF EXISTS `aluno_curso`;
+CREATE TABLE IF NOT EXISTS `aluno_curso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_curso` int(11) NOT NULL,
+  `id_aluno` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `aluno_curso`
+--
+
+INSERT INTO `aluno_curso` (`id`, `id_curso`, `id_aluno`) VALUES
+(1, 1, 1),
+(2, 3, 1),
+(3, 7, 1),
+(4, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aulas`
+--
+
+DROP TABLE IF EXISTS `aulas`;
+CREATE TABLE IF NOT EXISTS `aulas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_modulo` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `ordem` int(11) NOT NULL,
+  `tipo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `aulas`
+--
+
+INSERT INTO `aulas` (`id`, `id_modulo`, `id_curso`, `ordem`, `tipo`) VALUES
+(1, 1, 1, 1, 'video'),
+(2, 1, 1, 2, 'video'),
+(3, 2, 1, 1, 'video'),
+(4, 2, 1, 2, 'poll'),
+(5, 3, 1, 1, 'video'),
+(6, 3, 1, 2, 'video'),
+(7, 3, 1, 3, 'video');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cursos`
+--
+
+DROP TABLE IF EXISTS `cursos`;
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `imagem` varchar(37) NOT NULL,
+  `descricao` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `nome`, `imagem`, `descricao`) VALUES
+(1, 'PHP', 'php.jpg', NULL),
+(2, 'HTML', 'html.jpg', NULL),
+(3, 'JAVASCRIPT', 'javascript.jpg', NULL),
+(4, 'CSS', 'css.jpg', NULL),
+(9, 'Angular', 'c2c1873a9640d59813039ea230379476.jpg', 'Curso de Angular'),
+(6, 'jQuery', 'jquery.jpg', NULL),
+(7, 'Swift', 'swift.jpg', NULL),
+(8, 'Android', '0c79e62cc8139c123de03b0b22fbc111.jpg', 'Curso de Android 2.0');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `duvidas`
+--
+
+DROP TABLE IF EXISTS `duvidas`;
+CREATE TABLE IF NOT EXISTS `duvidas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data_duvida` datetime NOT NULL,
+  `respondida` tinyint(1) NOT NULL DEFAULT '0',
+  `duvida` text NOT NULL,
+  `id_aluno` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `duvidas`
+--
+
+INSERT INTO `duvidas` (`id`, `data_duvida`, `respondida`, `duvida`, `id_aluno`) VALUES
+(1, '2020-02-22 10:04:17', 0, 'duvida de teste', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `historico`
+--
+
+DROP TABLE IF EXISTS `historico`;
+CREATE TABLE IF NOT EXISTS `historico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `data_vista` datetime NOT NULL,
+  `id_aluno` int(11) NOT NULL,
+  `id_aula` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `historico`
+--
+
+INSERT INTO `historico` (`id`, `data_vista`, `id_aluno`, `id_aula`) VALUES
+(1, '2020-02-22 11:21:39', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `modulos`
+--
+
+DROP TABLE IF EXISTS `modulos`;
+CREATE TABLE IF NOT EXISTS `modulos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `modulos`
+--
+
+INSERT INTO `modulos` (`id`, `nome`, `id_curso`) VALUES
+(1, 'Básico', 1),
+(2, 'Intermediário', 1),
+(3, 'Avançado', 1),
+(11, 'Super Avançado 2', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `questionarios`
+--
+
+DROP TABLE IF EXISTS `questionarios`;
+CREATE TABLE IF NOT EXISTS `questionarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_aula` int(11) NOT NULL,
+  `pergunta` varchar(100) NOT NULL,
+  `opcao1` varchar(100) NOT NULL,
+  `opcao2` varchar(100) NOT NULL,
+  `opcao3` varchar(100) NOT NULL,
+  `opcao4` varchar(100) NOT NULL,
+  `resposta` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `questionarios`
+--
+
+INSERT INTO `questionarios` (`id`, `id_aula`, `pergunta`, `opcao1`, `opcao2`, `opcao3`, `opcao4`, `resposta`) VALUES
+(1, 4, 'Qual a pergunta ?', 'opção 1', 'opção 2', 'opção 3', 'opção 4', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `senha`) VALUES
+(1, 'suporte@fmlsistemas.com', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `videos`
+--
+
+DROP TABLE IF EXISTS `videos`;
+CREATE TABLE IF NOT EXISTS `videos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_aula` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `descricao` text,
+  `url` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `videos`
+--
+
+INSERT INTO `videos` (`id`, `id_aula`, `nome`, `descricao`, `url`) VALUES
+(1, 1, 'Aula 1', NULL, '391941242'),
+(2, 2, 'Aula 2', NULL, '391941242'),
+(3, 3, 'Aula 3', NULL, '391941242'),
+(4, 5, 'Aula 4', NULL, '391941242'),
+(5, 6, 'Aula 5', NULL, '391941242'),
+(6, 7, 'Aula 6', NULL, '391941242');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
